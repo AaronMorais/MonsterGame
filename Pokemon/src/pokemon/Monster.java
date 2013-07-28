60208db0a0760b1b717e1eb0f6905a663c697c86
mcpwns <matthewcheung520@hotmail.com>
2013-07-28 15:17:34 -0400

    Add attributes for Monster class

--- Pokemon/src/pokemon/Monster.java
+++ Pokemon/src/pokemon/Monster.java
@@ -8,6 +8,18 @@ package pokemon;
  *
  * @author Matt
  */
+
 public class Monster {
+    public enum MonsterType {
+        WATER, FIRE, GRASS, ELECTRIC, GROUND, FLYING
+    }
     
+    MonsterType type;
+    int hp; 
+    int level;
+    int attack;
+    int defense;
+    int speed;   
+    int[] moves;
+           
 }
