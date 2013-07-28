cac8874fbc27c5477f355149e797bfd789ddfe6f
mcpwns <matthewcheung520@hotmail.com>
2013-07-28 15:06:28 -0400

    Initial commit, add base classes

---
+++ Pokemon/src/pokemon/Pokemon.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author Matt
+ */
+public class Pokemon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
